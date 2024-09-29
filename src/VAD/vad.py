8017f4e1188daa098c8e1b525f48639b2afd1b51
Wajdzik <Zogfryt@gmail.com>
2024-09-29 06:51:55 +0200

    vpm implementation bitches!!!

--- src/VAD/vad.py
+++ src/VAD/vad.py
@@ -152,7 +152,7 @@ def vad_collector(sample_rate, frame_duration_ms,
             if num_unvoiced > 0.9 * ring_buffer.maxlen:
                 # sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
                 triggered = False
-                yield b''.join([f.bytes for f in voiced_frames])
+                yield (b''.join([f.bytes for f in voiced_frames]),f"{voiced_frames[0].timestamp}__{voiced_frames[-1].timestamp + voiced_frames[-1].duration}.wav")
                 ring_buffer.clear()
                 voiced_frames = []
     # if triggered:
@@ -165,7 +165,6 @@ def vad_collector(sample_rate, frame_duration_ms,
 
 
 def vad_find_silence(audio_path: str, directory_path: str):
-    print(audio_path)
     audio, sample_rate = vad_read_wave(audio_path)
     vad = webrtcvad.Vad(1)
     frames = frame_generator(30, audio, sample_rate)
@@ -176,10 +175,10 @@ def vad_find_silence(audio_path: str, directory_path: str):
         if isinstance(segment,Silence):
             silences.append(segment)
             # print(segment)
-        if isinstance(segment,bytes):
-            path = os.path.join(directory_path, f'chunk-{i:02}.wav')
+        else:
+            path = os.path.join(directory_path, segment[1])
             # print(' Writing %s' % (path,))
-            vad_write_wave(path, segment, sample_rate)
+            vad_write_wave(path, segment[0], sample_rate)
     return silences
         
 
